a6e4e8fdac8c21020f8e2240f933475492f81558
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-06 00:34:20 +0700

    👌👌👌Add Left Sidebar

--- app.vue
+++ app.vue
@@ -1,15 +1,22 @@
 <template>
   <div :class="{'dark': darkMode}">
-   <div class="min-h-full">
-    <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
-        <!-- left Sidebar -->
-        
-        <!-- Main Content -->
-        <!-- Right Sidebar -->
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+          <!-- left Sidebar -->
+          <div class="hidden md:block sm:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+          <!-- hwllo -->
+          <!-- Main Content -->
+          <!-- Right Sidebar -->
+        </div>
+      </div>   
     </div>
-   </div>  
   </div>
 </template>
 <script setup>
- const darkMode = ref(true)
+ const darkMode = ref(false)
 </script>
