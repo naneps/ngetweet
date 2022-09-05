033beac8f7f8e2bd32387255c1bc0d48c49484b0
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-05 22:17:02 +0700

    ➕Setup Dashboard

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div :class="{'dark': darkMode}">
+   <div class="min-h-full">
+    <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+        <!-- left Sidebar -->
+        
+        <!-- Main Content -->
+        <!-- Right Sidebar -->
+    </div>
+   </div>  
   </div>
 </template>
+<script setup>
+ const darkMode = ref(true)
+</script>
