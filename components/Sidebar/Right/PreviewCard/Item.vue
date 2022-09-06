6636d9eecc12c967192435e51b443fbff0cd9093
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-06 15:02:12 +0700

    ✅👌Item Right 👌✅

---
+++ components/Sidebar/Right/PreviewCard/Item.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="p-3 border-b hover:bg-gray-100 dark:hover:bg-dim-300" :class="wrapperClasses">
+        <slot ></slot>
+    </div>
+</template>
+<script setup>
+    const { defaultTransition  , twitterBorderColor} = useTailwindConfig();
+    const wrapperClasses = computed(()=> `${defaultTransition} ${twitterBorderColor}`)
+</script>
